09208db417a8f62f00a31bfbd778484f9593908a
MTanner <tannerwebdevelop@gmail.com>
2023-03-04 19:48:48 -0500

    Added play, pause functionality & audio controls

--- public/js/sound.js
+++ public/js/sound.js
@@ -3,20 +3,22 @@ const file = document.getElementById('fileSelect');
 const bg = document.getElementById('bgSelect');
 const body = document.getElementById('body');
 let canvas = document.getElementById('canvas');
+// const player = document.getElementById('audioPlayer');
 // let canvasRigt = document.getElementById('canvasRight');
+const audioCtx = new (window.AudioContext || window.webkitAudioContext);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 // const ctxRt = canvasRight.getContext('2d');
 // let audio1 = new Audio();
-// const audioCtx = new (window.AudioContext || window.webkitAudioContext);
 let audioSource = null;
 let analyser;
 let filepath;
 const basepath = './sounds/';
 let playing = false;
 let colors;
-let audio1 = new Audio();
+// let audio1 = new Audio();
+let audio1 = document.getElementById('audioPlayer');
 let count = 0;
 
 let warmColors = [
@@ -116,15 +118,72 @@ var methods = {
     } 
 };
 
-function playStop() {
-  audio1.src = filepath;
-  audio1.load();
-  const audioCtx = new (window.AudioContext || window.webkitAudioContext);
+function drawVisuals(audioCtx, analyser){
+  // const audioCtx = new (window.AudioContext || window.webkitAudioContext);
+  // audioSource = audioSource === null ? audioCtx.createMediaElementSource(audio1) : audioSource;
+  // analyser = audioCtx.createAnalyser();
+  // audioSource.connect(analyser);
+  
+  analyser.connect(audioCtx.destination);
+  analyser.fftSize = 2 ** 9;
+  // analyser.fftSize = 4096;
+  // const bufferLength = analyser.frequencyBinCount * 10;
+  const bufferLength = analyser.frequencyBinCount * 75;
+  const dataArray = new Uint8Array(bufferLength);
 
-    playing = !playing ? audio1.play() : audio1.pause();
+  // const barWidth = canvas.width/bufferLength;
+  const barWidth = (canvas.width/2)/bufferLength;
+  let barHeight;
+  let x;
+  let x2;
+  // animate(dataArray);
+
+  /***********
+   * Break the animate function out
+   * Call from color scheme selection
+   */
+
+  function animate(idx) {
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+
+    // Black Tips
+    methods[12](bufferLength, x, barWidth, barHeight, dataArray);
+    // Grey Bars
+    methods[11](bufferLength, x, barWidth, barHeight, dataArray);
+    // Black Bars
+    methods[10](bufferLength, x, barWidth, barHeight, dataArray);
+
+    requestAnimationFrame(animate);
+  }
+
+  //**************** File based index length */
+  // let idx = Math.floor(Math.random() * (file.files.length - 0 + 1)) + 0;
+
+  //**************** visualization methods length based */
+  let idx = Math.floor(Math.random() * (Object.keys(methods).length - 1 + 1)) + 0;
+  
+    console.log('index', idx);
+  animate(idx);
+}
+
+function playStop() {
+  // if(audio1.src === '')
+  // {
+  //   audio1.src = filepath;
+  //   audio1.load();
+  // }
+
+  // const audioCtx = new (window.AudioContext || window.webkitAudioContext);
+
+    // playing = !playing ? audio1.play() : audio1.pause();
 
     // may need to adjust
+    // drawVisuals();
+
     if(playing) {
+      const audioCtx = new (window.AudioContext || window.webkitAudioContext);
       audioSource = audioCtx.createMediaElementSource(audio1);
       analyser = audioCtx.createAnalyser();
       audioSource.connect(analyser);
